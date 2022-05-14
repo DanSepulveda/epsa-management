9ad41fc26121f106cbf310fc56768f094cac6ee3
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-14 13:12:03 -0400

    Upload sign image feature ok

--- client/src/components/admin/Signature.jsx
+++ client/src/components/admin/Signature.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
-import { errorMessage } from '../../utils/messages'
+import { errorMessage, successMessage } from '../../utils/messages'
 import IconButton from '../buttons/IconButton'
+import { storage } from '../../firebase.config'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { useDispatch, useSelector } from 'react-redux'
+import getExtension from '../../utils/getExtension'
+import { userState, editUser } from '../../redux/userSlice'
 
 const Signature = () => {
-    const [path, setPath] = useState('/assets/sad-minnie.png')
+    const state = useSelector(userState)
+    const { uid, token, signature } = state
+    const [path, setPath] = useState(signature || '/assets/sad-minnie.png')
+    const [loading, setLoading] = useState(false)
     const [file, setFile] = useState()
+    const dispatch = useDispatch()
+
 
     const inputHandler = (e) => {
         setFile(e.target.files[0])
@@ -12,13 +22,23 @@ const Signature = () => {
         setPath(newFile)
     }
 
-    const submit = () => {
-        if (!file) {
-            errorMessage('Debe seleccionar un archivo')
-        } else {
-            const fd = new FormData()
-            fd.append('signature', file)
-            console.log(fd)
+    const submit = async () => {
+        try {
+            if (!file) {
+                throw new Error('Debe seleccionar un archivo')
+            } else {
+                setLoading(true)
+                const extension = getExtension(file.name)
+                const imageRef = ref(storage, `users/${uid}/sign.${extension}`)
+                await uploadBytes(imageRef, file)
+                const url = await getDownloadURL(imageRef)
+                const values = { signature: url }
+                const response = await dispatch(editUser({ values, token }))
+                if (response.payload.success) successMessage('Firma actualizada')
+                setLoading(false)
+            }
+        } catch (error) {
+            errorMessage(error.message)
         }
     }
 
@@ -31,7 +51,9 @@ const Signature = () => {
                 id='signature'
                 onChange={inputHandler}
             />
-            <IconButton onClick={submit} icon='image'>Subir imagen</IconButton>
+            <IconButton onClick={submit} icon='image' loading={loading}>
+                Subir imagen
+            </IconButton>
         </div>
     )
 }
