c4b38c50f7f43caa593719f99069da36613d7dc2
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-16 14:40:02 -0400

    page edited

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import useTitle from '../hooks/useTitle'
 import AdminView from '../components/layout/AdminView'
-import Box from '../components/layout/Box'
 
 const Dashboard = () => {
     useTitle('Inicio')
