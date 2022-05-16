bc4522982d0dc8aeab41bdcd09bbd9fe1edab211
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-16 13:54:52 -0400

    error handler and styles updated

--- client/src/components/admin/Signature.jsx
+++ client/src/components/admin/Signature.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
-import { errorMessage, successMessage } from '../../utils/messages'
-import IconButton from '../buttons/IconButton'
+import { useState } from 'react'
 import { storage } from '../../firebase.config'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { useDispatch, useSelector } from 'react-redux'
-import getExtension from '../../utils/getExtension'
 import { userState, editUser } from '../../redux/userSlice'
+import getExtension from '../../utils/getExtension'
+import { loadingError, loadingMessage, loadingSuccess } from '../../utils/messages'
+import getErrorMsg from '../../app/getErrorMessage'
+import IconButton from '../buttons/IconButton'
+import themes from '../../app/themes'
 
 const Signature = () => {
+    const { text } = themes.default
     const state = useSelector(userState)
     const { uid, token, signature } = state
     const [path, setPath] = useState(signature)
     const [loading, setLoading] = useState(false)
     const [file, setFile] = useState()
     const dispatch = useDispatch()
-
 
     const inputHandler = (e) => {
         setFile(e.target.files[0])
@@ -24,21 +26,20 @@ const Signature = () => {
 
     const submit = async () => {
         try {
-            if (!file) {
-                throw new Error('Debe seleccionar un archivo')
-            } else {
-                setLoading(true)
-                const extension = getExtension(file.name)
-                const imageRef = ref(storage, `users/${uid}/sign.${extension}`)
-                await uploadBytes(imageRef, file)
-                const url = await getDownloadURL(imageRef)
-                const values = { signature: url }
-                const response = await dispatch(editUser({ values, token }))
-                if (response.payload.success) successMessage('Firma actualizada')
-                setLoading(false)
-            }
-        } catch (error) {
-            errorMessage(error.message)
+            if (!file) throw new Error('empty-file')
+            setLoading(true)
+            const toastId = loadingMessage('Actualizando firma')
+            const extension = getExtension(file.name)
+            const imageRef = ref(storage, `users/${uid}/sign.${extension}`)
+            await uploadBytes(imageRef, file)
+            const url = await getDownloadURL(imageRef)
+            const values = { signature: url }
+            const response = await dispatch(editUser({ values, token }))
+            setLoading(false)
+            if (response.payload.success) loadingSuccess('Actualizada', toastId)
+            else throw new Error(response.payload.response)
+        } catch ({ message }) {
+            loadingError(getErrorMsg(message))
         }
     }
 
@@ -48,7 +49,7 @@ const Signature = () => {
                 path
                     ? <img src={path} alt='Signature' className='h-28' />
                     : <div className='h-28 flex items-center justify-center'>
-                        <p className='text-pink-600 text-xl font-medium'>No ha subido ninguna imagen de firma</p>
+                        <p className={`text-xl font-medium ${text.sign}`}>No ha subido ninguna imagen</p>
                     </div>
             }
 
