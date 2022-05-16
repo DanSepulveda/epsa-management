59b67af098b92d3708426602c37fe1b8b3080a36
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-16 09:07:55 -0400

    Styles moved to config file

--- client/src/components/buttons/IconButton.jsx
+++ client/src/components/buttons/IconButton.jsx
@@ -1,18 +1,21 @@
 import { BsFileEarmarkWord, BsDownload, BsPlusLg, BsImage } from 'react-icons/bs'
+import themes from '../../app/themes'
 
 const IconButton = ({ children, icon, onClick, loading = false }) => {
+    const { buttons, common } = themes.default
+
     const icons = {
-        word: <BsFileEarmarkWord className='fill-white' />,
-        download: <BsDownload className='fill-white' />,
-        plus: <BsPlusLg className='fill-white' />,
-        image: <BsImage className='fill-white' />
+        word: <BsFileEarmarkWord className={buttons.iconFill} />,
+        download: <BsDownload className={buttons.iconFill} />,
+        plus: <BsPlusLg className={buttons.iconFill} />,
+        image: <BsImage className={buttons.iconFill} />
     }
 
     return (
         <button
             onClick={onClick}
             type='button'
-            className='flex items-center gap-2 py-1 px-2 rounded text-white transition-all duration-300 bg-pink-600 hover:bg-pink-600/90'
+            className={`flex items-center gap-2 py-1 px-2 rounded ${common.transition} ${buttons.iconBtn}`}
             disabled={loading ? true : false}
         >
             {icons[icon]}
