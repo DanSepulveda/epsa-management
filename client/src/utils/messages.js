e824e34819ebd8bb2edb28aec7d354c5a00dd124
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-16 19:37:56 -0400

    Loading toast added

--- client/src/utils/messages.js
+++ client/src/utils/messages.js
@@ -17,3 +17,32 @@ export const errorMessage = (message) => {
         }
     })
 }
+
+export const loadingMessage = (message) => {
+    return toast.loading(message, {
+        style: {
+            background: '#333',
+            color: '#fff'
+        }
+    })
+}
+
+export const loadingError = (message = "Ha ocurrido un problema. Intente más tarde", id) => {
+    return toast.error(message, {
+        style: {
+            background: '#333',
+            color: '#fff'
+        },
+        id
+    })
+}
+
+export const loadingSuccess = (message, id) => {
+    return toast.success(message, {
+        style: {
+            background: '#333',
+            color: '#fff'
+        },
+        id
+    })
+}
