2a631da3ca8a1b6f61aef8eb25d51db2cdb00607
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-16 20:03:10 -0400

    minor bugs fixed

--- client/src/app/errorMessages.js
+++
@@ -1,8 +0,0 @@
-const errorMessages = {
-    "auth/email-already-in-use": "Correo ya registrado",
-    "auth/user-not-found": "Correo y/o clave incorrecta",
-    "auth/wrong-password": "Correo y/o clave incorrecta",
-    "default": "Ha ocurrido un problema. Intente más tarde"
-}
-
-export default errorMessages
