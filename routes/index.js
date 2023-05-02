c05b6b32c884cfe5772d3a4ce5160e1cc45aedcf
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2023-05-02 17:42:55 -0400

    change get records method

--- routes/index.js
+++ routes/index.js
@@ -8,36 +8,36 @@ const reportsControllers = require('../controllers/reportsControllers')
 const router = express.Router()
 
 // USER CONTROLLERS
-router.route('/signup')
-    .get(validateUser, userControllers.createUser)
+router.route('/signup').get(validateUser, userControllers.createUser)
 
-router.route('/login')
-    .get(validateUser, userControllers.login)
+router.route('/login').get(validateUser, userControllers.login)
 
-router.route('/user')
-    .put(validateUser, userControllers.editUser)
+router.route('/user').put(validateUser, userControllers.editUser)
 
 // ACTIVITIES
-router.route('/activities')
-    .post(validateUser, activitiesControllers.createActivity)
-    .get(validateUser, activitiesControllers.getActivities)
+router
+  .route('/activities')
+  .post(validateUser, activitiesControllers.createActivity)
+  .get(validateUser, activitiesControllers.getActivities)
 
-router.route('/activity/:id')
-    .put(validateUser, activitiesControllers.editActivity)
-    .delete(validateUser, activitiesControllers.deleteActivity)
+router
+  .route('/activity/:id')
+  .put(validateUser, activitiesControllers.editActivity)
+  .delete(validateUser, activitiesControllers.deleteActivity)
 
 // RECORDS
-router.route('/records')
-    .post(validateUser, recordsControllers.createRecord)
-    .get(validateUser, recordsControllers.getRecords)
+router
+  .route('/records')
+  .post(validateUser, recordsControllers.createRecord)
+  .put(validateUser, recordsControllers.getRecords)
 
-router.route('/record/:id')
-    .put(validateUser, recordsControllers.editRecord)
-    .delete(validateUser, recordsControllers.deleteRecord)
+router
+  .route('/record/:id')
+  .put(validateUser, recordsControllers.editRecord)
+  .delete(validateUser, recordsControllers.deleteRecord)
 
 // REPORTS
 
-router.route('/monthly-report')
-    .post(validateUser, reportsControllers.monthlyReport)
+router.route('/monthly-report').post(validateUser, reportsControllers.monthlyReport)
 
 module.exports = router
