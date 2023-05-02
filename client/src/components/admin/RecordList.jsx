85549759d97807a8f9403e34e4528b5892aa2e27
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2023-05-02 17:44:43 -0400

    adding date input

--- client/src/components/admin/RecordList.jsx
+++ client/src/components/admin/RecordList.jsx
@@ -11,56 +11,81 @@ import RecordRow from './RecordRow'
 import themes from '../../app/themes'
 
 const RecordList = () => {
-    const { text } = themes.default
-    const dispatch = useDispatch()
-    const [open, setOpen] = useState(false)
-    const [tag, setTag] = useState('new')
-    const [id, setId] = useState(null)
-    const activities = useSelector(activityState).activities
-    const token = useSelector(userState).token
-    const { records, fetching, fetched } = useSelector(recordState)
+  const { text, common } = themes.default
+  const dispatch = useDispatch()
+  const [open, setOpen] = useState(false)
+  const [tag, setTag] = useState('new')
+  const [id, setId] = useState(null)
+  const activities = useSelector(activityState).activities
+  const token = useSelector(userState).token
+  const { records, fetching, fetched } = useSelector(recordState)
 
-    useEffect(() => {
-        if (!activities.length) dispatch(getActivities(token))
-        if (!records.length) dispatch(getRecords(token))
-        //eslint-disable-next-line
-    }, [])
+  useEffect(() => {
+    if (!activities.length) dispatch(getActivities(token))
+    const date = new Date()
+    const format = `${date.getFullYear()}-${date.getMonth() + 1}`
+    dispatch(getRecords({ token, date: format }))
+    //eslint-disable-next-line
+  }, [])
 
-    const data = tag === 'new' ? null : records.find(record => record._id === id)
+  const sendReq = (date) => {
+    dispatch(getRecords({ token, date }))
+  }
 
-    const message = fetched && !records.length
-        ? <InfoBox>Todavía no ha creado registros para el mes en curso</InfoBox>
-        : fetching
-            ? <InfoBox searching={true}>Buscando registros</InfoBox>
-            : null
+  const data = tag === 'new' ? null : records.find((record) => record._id === id)
 
-    useEffect(() => {
-        if (!open) setTag('new')
-    }, [open])
+  const message =
+    fetched && !records.length ? (
+      <InfoBox>Todavía no ha creado registros para el mes en curso</InfoBox>
+    ) : fetching ? (
+      <InfoBox searching={true}>Buscando registros</InfoBox>
+    ) : null
 
-    return (
-        <div className='flex flex-col'>
-            <div className='mt-6 mb-9 mx-auto flex items-center'>
-                <h2 className='text-center text-2xl mr-3'>{`Registros (${records.length})`}</h2>
-                <IconButton icon='plus' onClick={() => setOpen(true)}>
-                    Nuevo
-                </IconButton>
-            </div>
-            {message}
-            <div className='flex flex-col gap-2'>
-                {records.map(record => <RecordRow key={record._id} record={record} setOpen={setOpen} setTag={setTag} setId={setId} />)}
-            </div>
-            {
-                open &&
-                <OverForm setOpen={setOpen}>
-                    <h2 className={`text-center mb-7 text-2xl font-bold uppercase ${text.formTitle}`}>
-                        {tag === 'new' ? 'Crear registro' : 'Editar registro'}
-                    </h2>
-                    <RecordForm tag={tag} data={data} />
-                </OverForm>
-            }
-        </div>
-    )
+  useEffect(() => {
+    if (!open) setTag('new')
+  }, [open])
+
+  return (
+    <div className="flex flex-col">
+      <div className="mt-6 mb-9 mx-auto flex items-center">
+        <h2 className="text-center text-2xl mr-3">{`Registros (${records.length})`}</h2>
+        <IconButton
+          icon="plus"
+          onClick={() => setOpen(true)}
+        >
+          Nuevo
+        </IconButton>
+      </div>
+      <input
+        type="month"
+        onChange={(e) => sendReq(e.target.value)}
+        className={`py-1 px-2 mb-5 border ${common.inputBorder}`}
+      />
+      {message}
+      <div className="flex flex-col gap-2">
+        {records.map((record) => (
+          <RecordRow
+            key={record._id}
+            record={record}
+            setOpen={setOpen}
+            setTag={setTag}
+            setId={setId}
+          />
+        ))}
+      </div>
+      {open && (
+        <OverForm setOpen={setOpen}>
+          <h2 className={`text-center mb-7 text-2xl font-bold uppercase ${text.formTitle}`}>
+            {tag === 'new' ? 'Crear registro' : 'Editar registro'}
+          </h2>
+          <RecordForm
+            tag={tag}
+            data={data}
+          />
+        </OverForm>
+      )}
+    </div>
+  )
 }
 
 export default RecordList
